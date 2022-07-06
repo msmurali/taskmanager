3a953f5b200e3b4734fe207b2ed91ee954a438e6
muralims <muralikrishnan0511@gmail.com>
2022-07-06 11:46:08 +0530

    update app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Header from "components/header.component.jsx";
+import Header from "components/header.component";
+import SideBar from "components/sidebar.component";
 
 function App() {
   return (
-    <div className="app">
+    <div className="app min-h-screen grid grid-rows-layout">
       <Header />
+      <div className="w-100 grid grid-cols-layout">
+        <SideBar />
+      </div>
     </div>
   );
 }
