cd33cf502f0f721d14dd19aa1f10a44bc6821c3f
muralims <muralikrishnan0511@gmail.com>
2022-07-06 13:56:01 +0530

    update task.component

--- src/components/task.component.jsx
+++ src/components/task.component.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import ShoppingIcon from "components/icon.components/shopping.icon.component";
 import CheckIcon from "components/icon.components/check.icon.component";
+import ShoppingImg from "asset/img/shopping.png";
 
 const Task = () => {
   return (
-    <div className="task font-primary">
-      <div className="badge w-10 h-10 rounded-full border-2 border-white bg-purple-200 flex justify-center items-center">
+    <div className="task bg-slate-600 relative font-primary max-w-xs p-4 m-4 shadow-lg rounded-lg">
+      <div className="badge absolute -top-4 left-4 w-10 h-10 rounded-full border-2 border-white bg-purple-200 flex justify-center items-center">
         <ShoppingIcon color="rgb(126,34,206)" />
       </div>
-      <h1 className="task-title font-medium text-lg">Shop new furniture</h1>
-      <div className="task-img w-100 h-40"></div>
-      <div className="flex justify-between items-center">
+      <h1 className="task-title font-medium text-lg mt-5">
+        Shop new furniture
+      </h1>
+      <div className="task-img w-100 h-40 rounded my-4">
+        <img src={ShoppingImg} alt="shopping" className="object-cover" />
+      </div>
+      <div className="flex justify-between items-center mb-4">
         <div className="tag text-sm px-3 py-1 rounded font-medium bg-purple-300 text-purple-700">
           Shopping
         </div>
@@ -18,8 +23,8 @@ const Task = () => {
           21 Jun - 30 Jun
         </div>
       </div>
-      <div className="grid grid-cols-2">
-        <div className="progress">
+      <div className="flex justify-between items-center">
+        <div className="progress w-full">
           <div className="flex justify-between items-center mb-1">
             <span className="text-sm">Progress</span>
             <span className="text-sm">50%</span>
@@ -29,7 +34,7 @@ const Task = () => {
             <div className="w-100 h-2 bg-gray-200 rounded"></div>
           </div>
         </div>
-        <div className="tasks-count flex items-center">
+        <div className="tasks-count ml-10 flex items-center">
           <span className="inline mr-2">
             <CheckIcon />
           </span>
