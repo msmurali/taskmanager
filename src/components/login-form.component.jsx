f2833ec2844dbec61481869491aba593a523876d
muralims <muralikrishnan0511@gmail.com>
2022-07-07 16:39:12 +0530

    update login-form component

--- src/components/login-form.component.jsx
+++ src/components/login-form.component.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { login } from "services/auth";
 
 const LoginForm = () => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [error, setError] = React.useState(null);
 
   const changeHandler = (e) => {
     if (e.target.name === "email") {
@@ -14,7 +16,22 @@ const LoginForm = () => {
     }
   };
 
-  const submitHandler = () => {};
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      await login(email, password);
+    } catch (error) {
+      if (error.code === "auth/user-not-found") {
+        setError("User not found");
+      } else if (error.code === "auth/wrong-password") {
+        setError("Invalid email or password");
+      } else {
+        setError("Something went wrong");
+      }
+    }
+  };
 
   return (
     <div className="login-form w-full h-full overflow-y-auto font-primary">
@@ -23,6 +40,11 @@ const LoginForm = () => {
         className="w-full h-full p-6 md:w-1/2 mx-auto flex flex-col justify-evenly items-stretch"
       >
         <h1 className="title font-medium text-lg mt-8 text-center">Login</h1>
+        {error && (
+          <span className="text-red-500 text-sm px-4 py-2 bg-red-200 mt-8 rounded">
+            {error}
+          </span>
+        )}
         <div className="form-group mt-8">
           <label htmlFor="email" className="block text-sm mb-1">
             Email
@@ -44,6 +66,7 @@ const LoginForm = () => {
           <input
             type="password"
             name="password"
+            minLength="6"
             id="password"
             className="bg-gray-100 w-full px-4 py-2 border-2 border-gray-200 focus:border-purple-700 outline-none rounded-md"
             required
