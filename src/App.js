be0ae7ffa6368824bdbd0e8abeb0644ef71ffb0e
muralims <muralikrishnan0511@gmail.com>
2022-07-07 12:39:58 +0530

    create register-from.component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Dashboard from "components/dashboard.component";
 import Form from "components/task-form.component";
+import LoginForm from "components/login-form.component";
 import Header from "components/header.component";
 import SideBar from "components/sidebar.component";
 import "./app.css";
@@ -8,10 +9,11 @@ function App() {
   return (
     <div className="app h-screen mx-auto">
       <Header />
-      <div className="main w-100 grid grid-cols-1 grid-rows-layout md:grid-rows-1 md:grid-cols-layout">
+      {/* <div className="main w-100 grid grid-cols-1 grid-rows-layout md:grid-rows-1 md:grid-cols-layout">
         <SideBar />
         <Dashboard />
-      </div>
+      </div> */}
+      <LoginForm />
     </div>
   );
 }
---
+++ src/components/register-form.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegisterForm = () => {
+  return <div className="register-form w-full"></div>;
+};
+
+export default RegisterForm;
