e06cdd74a56382050587e4b74c1cff4d67f9153d
muralims <muralikrishnan0511@gmail.com>
2022-07-07 10:41:53 +0530

    create enums.js

---
+++ src/constants/enums.js
@@ -0,0 +1,9 @@
+export const Tag = Object.freeze({
+  GENERAL: "general",
+  ENTERTAINMENT: "entertainment",
+  LEARNING: "learning",
+  SHOPPING: "shopping",
+  TRAVEL: "travel",
+  URGENT: "urgent",
+  WORK: "work",
+});
