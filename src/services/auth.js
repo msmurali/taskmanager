553fd8bfda346814602d473700ba51c768553937
muralims <muralikrishnan0511@gmail.com>
2022-07-07 15:58:36 +0530

    update auth service

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,3 +6,11 @@ export const auth = app.auth();
 export const register = (email, password) => {
   return auth.createUserWithEmailAndPassword(email, password);
 };
+
+export const login = (email, password) => {
+  return auth.signInWithEmailAndPassword(email, password);
+};
+
+export const logout = () => {
+  return auth.signOut();
+};
