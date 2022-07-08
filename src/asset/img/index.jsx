f5e4f001fdd84b0d98f85dddaa383b7e65d4c88a
muralims <muralikrishnan0511@gmail.com>
2022-07-08 11:59:02 +0530

    create index for asset/img

---
+++ src/asset/img/index.jsx
@@ -0,0 +1,17 @@
+import GeneralImg from "./general.png";
+import EntertainmentImg from "./entertainment.png";
+import ShoppingImg from "./shopping.png";
+import UrgentImg from "./urgent.png";
+import LearningImg from "./learning.png";
+import TravelImg from "./travel.png";
+import WorkImg from "./work.png";
+
+export {
+  GeneralImg,
+  EntertainmentImg,
+  ShoppingImg,
+  UrgentImg,
+  LearningImg,
+  TravelImg,
+  WorkImg,
+};
