66d5d124de600eca45b0ed85a798cdf5b182db56
muralims <muralikrishnan0511@gmail.com>
2022-07-08 11:58:31 +0530

    create index for icon.components

---
+++ src/components/icon.components/index.jsx
@@ -0,0 +1,33 @@
+import AddIcon from "./add.icon.component";
+import CheckIcon from "./check.icon.component";
+import CloseIcon from "./close.icon.component";
+import EntertainmentIcon from "./entertainment.icon.component";
+import GearIcon from "./gear.icon.component";
+import GeneralIcon from "./general.icon.component";
+import LearningIcon from "./learning.icon.component";
+import LogoutIcon from "./logout.icon.component";
+import SearchIcon from "./search.icon.component";
+import SettingsIcon from "./settings.icon.component";
+import ShoppingIcon from "./shopping.icon.component";
+import TasksIcon from "./tasks.icon.component";
+import TravelIcon from "./travel.icon.component";
+import UrgentIcon from "./urgent.icon.component";
+import WorkIcon from "./work.icon.component";
+
+export {
+  AddIcon,
+  CheckIcon,
+  CloseIcon,
+  EntertainmentIcon,
+  GearIcon,
+  GeneralIcon,
+  LearningIcon,
+  LogoutIcon,
+  SearchIcon,
+  SettingsIcon,
+  ShoppingIcon,
+  TasksIcon,
+  TravelIcon,
+  UrgentIcon,
+  WorkIcon,
+};
