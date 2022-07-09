29eb62a6b5862f8a3a505e1b4ab08c8c9febf0af
muralims <muralikrishnan0511@gmail.com>
2022-07-09 13:08:01 +0530

    create router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
+import { Router } from "react-router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
