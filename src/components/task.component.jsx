7b95275232cf5d51d8542e0a0fcf943b661960a2
muralims <muralikrishnan0511@gmail.com>
2022-07-14 15:32:09 +0530

    create route links

--- src/components/task.component.jsx
+++ src/components/task.component.jsx
@@ -4,6 +4,7 @@ import { CheckIcon, CloseIcon } from "components/icon.components/index";
 import Badge from "components/badge.component";
 import { removeTask } from "services/tasks";
 import { useAuth } from "contexts/auth-context";
+import { Link } from "react-router-dom";
 
 const Tag = ({ tag }) => {
   return (
@@ -34,7 +35,7 @@ const DateTag = ({ from, to }) => {
   );
 };
 
-const Task = ({ task }) => {
+const Task = ({ task, id }) => {
   const { user } = useAuth();
 
   const remove = async () => {
@@ -42,54 +43,58 @@ const Task = ({ task }) => {
   };
 
   return (
-    <div className="task mx-auto mt-10 bg-white relative font-primary max-w-xs p-4 m-4 shadow-lg rounded-lg border border-gray-200">
-      <Badge tag={task.tag} />
-      <div className="flex justify-between items-center mt-5">
-        <h1 className="task-title font-regular text-base">
-          {task && task.title}
-        </h1>
-        <button className="bg-transparent p-2" onClick={remove}>
-          <CloseIcon color="red" />
-        </button>
-      </div>
-      <div
-        className="task-img w-100 h-40 rounded-lg my-4 bg-cover"
-        style={{
-          backgroundImage: `url(${TagImg[task.tag.toUpperCase()]})`,
-        }}
-      ></div>
-      <div className="flex justify-between items-center mb-4 pt-3">
-        <Tag tag={task.tag} />
-        <DateTag from={task.from} to={task.to} />
-      </div>
-      <div className="flex justify-between items-end">
-        <div className="progress w-full">
-          <div className="flex justify-between items-center mb-1">
-            <span className="text-sm">Progress</span>
-            <span className="text-sm">
-              {`${Math.round((task.completedTasks / task.totalTasks) * 100)}%`}
-            </span>
-          </div>
-          <div className="progress-bar relative">
-            <div
-              className="h-2 bg-green-500 absolute rounded"
-              style={{
-                width: `${Math.round(
+    <Link to={`/task/${id}`}>
+      <div className="task mx-auto mt-10 bg-white relative font-primary max-w-xs p-4 m-4 shadow-lg rounded-lg border border-gray-200 cursor-pointer">
+        <Badge tag={task.tag} />
+        <div className="flex justify-between items-center mt-5">
+          <h1 className="task-title font-regular text-base">
+            {task && task.title}
+          </h1>
+          <button className="bg-transparent p-2" onClick={remove}>
+            <CloseIcon color="red" />
+          </button>
+        </div>
+        <div
+          className="task-img w-100 h-40 rounded-lg my-4 bg-cover"
+          style={{
+            backgroundImage: `url(${TagImg[task.tag.toUpperCase()]})`,
+          }}
+        ></div>
+        <div className="flex justify-between items-center mb-4 pt-3">
+          <Tag tag={task.tag} />
+          <DateTag from={task.from} to={task.to} />
+        </div>
+        <div className="flex justify-between items-end">
+          <div className="progress w-full">
+            <div className="flex justify-between items-center mb-1">
+              <span className="text-sm">Progress</span>
+              <span className="text-sm">
+                {`${Math.round(
                   (task.completedTasks / task.totalTasks) * 100
-                )}%`,
-              }}
-            ></div>
-            <div className="w-100 h-2 bg-gray-200 rounded"></div>
+                )}%`}
+              </span>
+            </div>
+            <div className="progress-bar relative">
+              <div
+                className="h-2 bg-green-500 absolute rounded"
+                style={{
+                  width: `${Math.round(
+                    (task.completedTasks / task.totalTasks) * 100
+                  )}%`,
+                }}
+              ></div>
+              <div className="w-100 h-2 bg-gray-200 rounded"></div>
+            </div>
+          </div>
+          <div className="tasks-count ml-10 flex items-center">
+            <span className="inline mr-2">
+              <CheckIcon />
+            </span>
+            <span className="inline">{`${task.completedTasks}/${task.totalTasks}`}</span>
           </div>
         </div>
-        <div className="tasks-count ml-10 flex items-center">
-          <span className="inline mr-2">
-            <CheckIcon />
-          </span>
-          <span className="inline">{`${task.completedTasks}/${task.totalTasks}`}</span>
-        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
