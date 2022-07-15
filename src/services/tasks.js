5ee7436e847007c4c1eeb9973f1610851d22afe2
muralims <muralikrishnan0511@gmail.com>
2022-07-15 16:15:30 +0530

    update tasks service

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -6,39 +6,46 @@ import {
   updateDoc,
   arrayUnion,
   arrayRemove,
+  deleteField,
 } from "firebase/firestore";
+import { v4 as uuid } from "uuid";
 
 export const db = getFirestore(app);
 export const col = "tasks-collection";
 
 export const addTask = async (uid, data) => {
-  const key = `tasks-${uid}`;
+  const key = uuid();
   try {
     await updateDoc(doc(db, col, uid), {
-      [key]: arrayUnion(data),
+      [key]: data,
     });
   } catch (error) {
     if (error.code === "not-found") {
-      await setDoc(doc(db, col, uid), { [`tasks-${uid}`]: [data] });
+      await setDoc(doc(db, col, uid), { [key]: data });
     } else {
       console.log(error);
     }
   }
 };
 
-export const updateTask = async (uid, task) => {
+export const updateTask = async (uid, oldData, newData) => {
+  const key = `tasks-${uid}`;
   try {
-    await updateDoc(doc(db, col, uid), task);
+    await updateDoc(doc(db, col, uid), {
+      [key]: arrayRemove(oldData),
+    });
+    await updateDoc(doc(db, col, uid), {
+      [key]: arrayUnion(newData),
+    });
   } catch (error) {
     console.log(error);
   }
 };
 
-export const removeTask = async (uid, data) => {
-  const key = `tasks-${uid}`;
+export const removeTask = async (uid, taskId) => {
   try {
     await updateDoc(doc(db, col, uid), {
-      [key]: arrayRemove(data),
+      [taskId]: deleteField(),
     });
   } catch (error) {
     console.log(error);
